0fc9062c6105f01340388dc05772ca67ad6a7eb6
jralph <jralph@affinitysolutions.com>
2015-12-07 17:15:13 -0500

    Changed function name to lowercase and added 2 spaced for PEP compliance.

--- mysql_learning/mysql_connect.py
+++ mysql_learning/mysql_connect.py
@@ -1,5 +1,6 @@
 from mysql import connector
 import optparse
+
 ## Jason R.
 
 parser = optparse.OptionParser()
@@ -9,21 +10,24 @@ parser.add_option('-d', action="store", help="Enter Department", type="string")
 parser.add_option('-s', action="store", help="Enter Salary", type="int")
 options, args = parser.parse_args()
 
-def databaseAdd(id,name,dept,salary):
+
+def database_add(id, name, dept, salary):
     try:
         con = connector.Connect(user='jason',
-                            password='trace3',
-                            database='jason',
-                            host='localhost')
+                                password='trace3',
+                                database='jason',
+                                host='localhost')
         c = con.cursor()
         table_name = "employee"
         insert = "INSERT INTO " + table_name + " (id,name,dept,salary) \
         VALUES (%s, %s, %s, %s)"
-        data_value = (id,name,dept,salary)
+        data_value = (id, name, dept, salary)
         c.execute(insert, data_value)
         con.commit()
         c.close()
         con.close()
     except:
         print('Can\'t connect to the database')
-databaseAdd(options.i, options.n, options.d, options.s)
+
+
+database_add(options.i, options.n, options.d, options.s)
